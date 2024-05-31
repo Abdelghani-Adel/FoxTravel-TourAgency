e24f09718359c5a8e6d3fac9b70c2b26c0060f05
abdelghany <abdelghany@rockettravelsystem.com>
2024-05-31 16:55:01 +0300

    update

--- app/(routes)/contact/_branchList.tsx
+++ app/(routes)/contact/_branchList.tsx
@@ -3,47 +3,49 @@ import BranchDetails from "./_branchDetails";
 
 const BranchList = () => {
   return (
-    <div className="row justify-content-evenly">
-      <div className="mb-4">
+    <>
+      <div className="mb-4 col-12">
         <h2 className="text-center fw-normal mb-2 fs-1">Get in touch</h2>
         <p className="text-center fw-normal textGray">
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
         </p>
       </div>
 
-      <div className="col-3">
-        <BranchDetails
-          title="Hurghada"
-          email="Hrg.office@foxtraveleg.com"
-          phone="+201028886667"
-          address="147 El-Kawther , Airport Section / Hurghada – Red Sea , Egypt"
-          workingDays="Saturday - Thursday"
-          workingHours="10:00 - 16:00"
-        />
-      </div>
+      <div className="row row-cols-1 row-cols-lg-3 justify-content-evenly">
+        <div className="col">
+          <BranchDetails
+            title="Hurghada"
+            email="Hrg.office@foxtraveleg.com"
+            phone="+201028886667"
+            address="147 El-Kawther , Airport Section / Hurghada – Red Sea , Egypt"
+            workingDays="Saturday - Thursday"
+            workingHours="10:00 - 16:00"
+          />
+        </div>
 
-      <div className="col-3">
-        <BranchDetails
-          title="Cairo"
-          email="Hrg.office@foxtraveleg.com"
-          phone="+201028886667"
-          address="147 El-Kawther , Airport Section / Hurghada – Red Sea , Egypt"
-          workingDays="Saturday - Thursday"
-          workingHours="10:00 - 16:00"
-        />
-      </div>
+        <div className="col">
+          <BranchDetails
+            title="Cairo"
+            email="Hrg.office@foxtraveleg.com"
+            phone="+201028886667"
+            address="147 El-Kawther , Airport Section / Hurghada – Red Sea , Egypt"
+            workingDays="Saturday - Thursday"
+            workingHours="10:00 - 16:00"
+          />
+        </div>
 
-      <div className="col-3">
-        <BranchDetails
-          title="Sharm"
-          email="Hrg.office@foxtraveleg.com"
-          phone="+201028886667"
-          address="147 El-Kawther , Airport Section / Hurghada – Red Sea , Egypt"
-          workingDays="Saturday - Thursday"
-          workingHours="10:00 - 16:00"
-        />
+        <div className="col">
+          <BranchDetails
+            title="Sharm"
+            email="Hrg.office@foxtraveleg.com"
+            phone="+201028886667"
+            address="147 El-Kawther , Airport Section / Hurghada – Red Sea , Egypt"
+            workingDays="Saturday - Thursday"
+            workingHours="10:00 - 16:00"
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
